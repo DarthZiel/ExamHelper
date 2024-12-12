8361b60ea32c8adea0ddd3617a4dd1c1ffc00a20
DarthZiel <Studerzhan@gmail.com>
2024-12-12 12:35:21 +0500

    first commit

---
+++ exam/models.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = (
+        ('teacher', 'Teacher'),
+        ('student', 'Student'),
+    )
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='student')
+
+    def __str__(self):
+        return f"{self.username} ({self.get_role_display()})"
